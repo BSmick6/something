8b86d7aed199742489f30209c651ad41cff0b1f1
Brock <bws219@lehigh.edu>
2017-10-21 01:57:32 -0700

    cleanup

--- screens/SwiperScreen.js
+++ screens/SwiperScreen.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import CreateEventScreen from './CreateEventScreen';
 import ImagePickerScreen from './ImagePickerScreen';
+import SaveScreen from './SaveScreen';
 import Swiper from 'react-native-swiper';
 import styles from '../styles/styles.js';
+const domain = "https://something-horizons.herokuapp.com";
 
 class SwiperScreen extends React.Component {
   static navigationOptions = {
@@ -11,11 +13,27 @@ class SwiperScreen extends React.Component {
     headerStyle: { marginTop: 25 },
   };
 
+  componentWillUnmount() {
+    fetch(`${domain}/logout`)
+    .then(resp => resp.json())
+    .then(respJson => {
+      if (respJson.success) {
+        console.log(respJson.message);
+      } else {
+        console.log('ERROR LOGGING OUT');
+      }
+    })
+    .catch(err => {
+      console.log('ERROR', err);
+    })
+  }
+
   render() {
     return (
       <Swiper style={styles.container} showsPagination={false}>
         <CreateEventScreen />
         <ImagePickerScreen />
+        <SaveScreen />
       </Swiper>
     )
   }
